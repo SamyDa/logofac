4eb8aac14312ecb121e0196b498654b23bd13be1
samyd <samyd@192.168.1.62>
2020-08-31 15:33:33 +0200

    Initial project

---
+++ LogoFac/src/main/java/be/logofac/LogoFac/LogoFacApplication.java
@@ -0,0 +1,13 @@
+package be.logofac.LogoFac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogoFacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogoFacApplication.class, args);
+	}
+
+}
---
+++ LogoFac/src/test/java/be/logofac/LogoFac/LogoFacApplicationTests.java
@@ -0,0 +1,13 @@
+package be.logofac.LogoFac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogoFacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
